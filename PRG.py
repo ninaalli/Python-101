bbe36fdd6d762b86676c8891cf7225d5584d79bf
ninaalli <ninaalli@users.noreply.github.com>
2016-04-21 17:08:17 -0400

    Create PRG.py

---
+++ PRG.py
@@ -0,0 +1,35 @@
+from view import class ClassName(object):
+    """this will allow user to sign up and log in """
+    def __init__(self,name):
+        super(user, self).__init__()
+        self.name = name
+        #there is no password provided
+        input ("What yo name be?")
+
+    def __init__(self.lucky):
+        input = ("Would you like a trick or treat?")
+        #user will write trick and specific names output
+        #user will write treat and specific names output
+        choice = input ["Nothing [0], Trick [1], Treat [2]"]
+            if "Nothing" end game
+            if "Trick", select.name.Trick
+            if "Treat", select.name.Treat
+
+            #names of the Trick and Treat hoeses
+        Trick = input ["Derpy Hooves [0], Princess Luna [1], Discord [3]"]
+        Treat = input ["Rainbow Dash [0], Rarity [2], Sunset Shimmer [3]"]
+
+    def __init__(self.ready):
+        input = ("Will you be providing insurance?")
+            if ["No" [0]], end game
+                elif ("You mean Yes?")
+            if ["Yes" [1]], continue to patdown
+
+    def __init__(self.pay)
+        input = ("Where's da money?")
+        if [credit card [0]]:
+            pass
+        if [cash [1]]:
+            pass
+        if [coins [2]]:
+            end game
