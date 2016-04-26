f64001049e3790e6423ea4afc4afb5e2f6fd36ba
ninaalli <ninaalli@users.noreply.github.com>
2016-04-26 11:03:43 -0400

    Create Week3Day2.py

---
+++ Week3Day2.py
@@ -0,0 +1,19 @@
+String and List are iterable.
+
+Week#/Day1For DB-why would you need more than one instance?
+
+SQL JOINS
+*#1 thing that slows down a website is the db querying*
+
+Foreign Keys go to the MANY instance
+id INCREMENT PRIMARY KEY AUTOINCREMENT will auto add the key for you.
+
+print("Destroying old data") - it will delete the info that is already in there
+
+type cat in command line to bring in items from Bash/Terminal
+
+Join is to hit two tables at once and get one result
+INNER JOIN - commonly used,you have to tell it the foreign key
+
+Look at the various SQL joins
+
