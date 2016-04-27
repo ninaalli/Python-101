b579e23e8614f582f0ff04e77ff132c4d4f65146
ninaalli <ninaalli@users.noreply.github.com>
2016-04-27 11:20:43 -0400

    Update Week3Day2.py

--- Week3Day2.py
+++ Week3Day2.py
@@ -18,4 +18,47 @@ INNER JOIN - commonly used,you have to tell it the foreign key
 Look at the various SQL joins: http://www.codeproject.com/Articles/33052/Visual-Representation-of-SQL-Joins
 
 
-
+Example Code:
+controller
+​
+class User:
+​
+    def login:
+        checks to see if the un and pw is in db
+        returns t if theyre there
+        false is theyre now
+​
+    def get permission
+        another call to db to get permission
+        returns 1 if client
+        returns 2 if bankers
+        returns None if neither
+​
+​
+    def welcomescreen_banker
+        shows banker menu
+        just call a view function
+        potentiially get back a choice
+​
+    def welcomescreen_client
+        shows banker menu
+        just call a view function
+        potentiially get back a choice
+​
+controller
+​
+class Run:
+​
+    def run
+        while login != true
+            give some error message
+            login
+​
+        permission = get_permission
+​
+        if permission == 1
+            welcomescreen_clienbt
+​
+        elif permission == 2
+            welcomescreen_client
+​
