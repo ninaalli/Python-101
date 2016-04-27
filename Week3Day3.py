f1ba5c3da1deec5355adfa0917da9f2e56e79ec2
ninaalli <ninaalli@users.noreply.github.com>
2016-04-27 10:10:44 -0400

    Update Week3Day3.py

--- Week3Day3.py
+++ Week3Day3.py
@@ -1,1 +1,12 @@
 API (application program interface) is a set of routines, protocols, and tools for building software applications. The API specifies how software components should interact and APIs are used when programming graphical user interface (GUI) components.
+
+To the extent that systems conform to the constraints of REST they can be called RESTful. RESTful systems typically, but not always, communicate over Hypertext Transfer Protocol (HTTP) with the same HTTP verbs (GET, POST, PUT, DELETE, etc.) that web browsers use to retrieve web pages and to send data to remote servers.[4] REST systems interface with external systems as web resources identified by Uniform Resource Identifiers (URIs), for example /people/tom, which can be operated upon using standard verbs such as DELETE /people/tom.
+
+In computer programming, create, read, update and delete[1] (as an acronym CRUD) (Sometimes called SCRUD with an "S" for Search) are the four basic functions of persistent storage.[2] Sometimes CRUD is expanded with the words retrieve instead of read, modify instead of update, or destroy instead of delete. It is also sometimes used to describe user interface conventions that facilitate viewing, searching, and changing information; often using computer-based forms and reports. 
+
+Operation	SQL	HTTP	DDS
+Create	INSERT	PUT / POST	write
+Read (Retrieve)	SELECT	GET	read / take
+Update (Modify)	UPDATE	POST / PUT / PATCH	write
+Delete (Destroy)	DELETE	DELETE	dispose
+
