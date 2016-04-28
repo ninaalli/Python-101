ba629b0516c4e5576e3f21063036e6c7d4eae4d1
ninaalli <ninaalli@users.noreply.github.com>
2016-04-28 11:49:18 -0400

    Create Week3Day4.py

---
+++ Week3Day4.py
@@ -0,0 +1,22 @@
+
+Part I - What is a wrapper? (not rapper)
+A wrapper function is making your code more efficient and DRY (keep it in one language) by wrapping it in a method we can invoke when we need it.
+In terms of Python and APIs we want to wrap our api calls in a method so we don't have to fill our code repeating endpoints
+when we built SQL and it was wrapped in the Python language - 
+
+APIs and wrappers
+
+API keys are used to limit who and how many requests can be made in a given time frame.
+
+Part II - API Limitations/API Keys
+API key requirement
+API keys are used to limit who and how many requests can be made in a given time frame
+When you get a key to an API make sure to see if there are limitations and what those limitations are
+
+Part III - Gitignore and Environmental Variables
+You can create a gitignore file and choose which files to not push to github
+You can also use Environmental Variables.
+
+Part IV - Brief Extension of RESTful API Practices
+
+
