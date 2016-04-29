def34b7d4f4b18f495e7a89b251b22ea32855172
ninaalli <ninaalli@users.noreply.github.com>
2016-04-29 16:18:47 -0400

    Create Week1Day5.py

---
+++ Week1Day5.py
@@ -0,0 +1,29 @@
+Using Git
+
+Git is a collaboration tool that is universally used by programmers
+Below are the commands in git we will be using the most:
+git init - initialize a connection between that folder and git
+git add - show off your updates / differences between the repos
+git commit -m "message here" - add a message to this instance
+git pull origin branch_name - pull down the updates/changes from a repository
+git push origin branch_name - push up your changes to a repository
+git checkout -b branch_name - change to a different branch on your local machine, or create a new branch on your local machine
+git status - tells you what changes are in this folder and what branch you are on
+git clone - makes a copy of a targeted repo in a specified folder of your choice
+
+repos will be done with branches
+
+Mutable vs. Immutable
+Mutable = Can be changed / mutated
+  Lists
+  Dictionaries
+Immutable = Cannot be changed or mutated
+  Strings
+  Tuples
+  
+Inheritance and Scope
+Think about the accronym LEGB
+  Local
+  Enclosing
+  Global
+  Built In
