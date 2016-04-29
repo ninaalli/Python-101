71788881491808468f4d35ea8f2a67d40305f56b
ninaalli <ninaalli@users.noreply.github.com>
2016-04-29 15:42:52 -0400

    Create Week1Day3.py

---
+++ Week1Day3.py
@@ -0,0 +1,37 @@
+Object Oriented Programming
+  Day 1
+  
+Python is an object oriented language.
+It allows us to organize our code for efficiency and readability
+
+Object Oriented Programming is the practice of writing your code around Objects
+
+What are Objects?
+Everything in Python is an object
+Every object is an instance of a class
+We know about the various data types and data structures. Well take lists for example. The moment you create a list you made an instance of it. This list now has access to all the methods inside the list class of Python3
+lists belong to a list class
+strings belong to a string class
+dictionaries belong to a dictionary class
+
+Classes
+  A class holds many methods that an object can respond to.
+They are defined with the word class
+They are always capitalized
+They are just blueprints for us to use later
+
+Classes Terminology:
+  Instantiate - when we instantiate a class we are creating a new instance of that class.
+  Class variable is a variable you want to be given to every instance of the class
+  Instance variable is specific to that instance of the class
+  *The term self is used over and over again throughout class creation. This is referring to the object in that moment in time. (Instance of the object)
+
+Scope: 
+  Scope works inside out
+  The methods and variables that are local will have access to those outside of it, however, the outside does not have access to the inside.
+  Think about Russian Nesting Dolls
+  return is what allows us to move values and variables between two different things
+
+To comment out your code: 
+triple quotes doc string used to comment your code and help others read your code 
+or #in front of code 
