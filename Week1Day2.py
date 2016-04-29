1062f7b7df0b357d1482f48e006e99f78ff76af1
ninaalli <ninaalli@users.noreply.github.com>
2016-04-29 15:11:16 -0400

    Create Week1Day2.py

---
+++ Week1Day2.py
@@ -0,0 +1,41 @@
+Variables are a way to store and save data for use
+This is called assignment. You are assigning a value to a variable
+Declaring Variables
+Do not need to use var
+Cannot start with a number
+Cannot declare with special characters
+Written in snake case
+
+Data Types
+Strings
+Strings are immutable. Once they are declared they cannot be changed. You can add strings together to create a new string but you cannot mutate an already existing one
+  Numbers
+    Floats
+      decimals
+    Integers
+      whole number
+  Booleans
+    0
+    False
+    
+    Supports PEMDAS
+==, !=, <=, >=, <, >
+
+if/else statements
+  If an expression you passed in is True do something
+  Else do something else
+
+Acknowledge:  
+You don't need parenthesis
+Put a semi colon after the expression you want to evaluate
+if to elif to else
+Tab to show what part of the function belongs where
+
+
+  
+  
+  
+  
+  
+  
+  
