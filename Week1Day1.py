aa76464683ea17f4e45d8999486d116819af45f9
ninaalli <ninaalli@users.noreply.github.com>
2016-04-29 14:53:51 -0400

    Update Week1Day1.py

--- Week1Day1.py
+++ Week1Day1.py
@@ -7,3 +7,44 @@ git pull origin branch_name - pull down the updates/changes from a repository
 git push origin branch_name - push up your changes to a repository
 git checkout -b branch_name - change to a different branch on your local machine, or create a new branch on your local machine
 git status - tells you what changes are in this folder and what branch you are on
+
+
+common commands
+ls - list the files in the folder
+mkdir - make directory, make a folder
+touch - make a file
+pwd - print working directory, this will give you the path in your computer where you currently are
+cd - change directory
+cd by itself will bring you to your root folder
+cd .. will bring you up one folder
+cd folder_name will take you to that folder
+
+Instance Methods
+.get - "acts as is key in this dictionary"
+.values - gives a list of values
+.keys - gives a list of keys
+.items - gives the keys and values as tuples in a list
+
+Python's built in functions
+len()
+range()
+split()
+pop()
+append()
+
+DateTime
+import datetime
+ 
+from datetime import datetime
+
+
+Built in functions:
+zip()
+recursive functions
+with file_name as name_for_code:
+  
+
+Read Files: Identify them
+.read()
+.readline()
+.readlines()
