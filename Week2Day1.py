aa0a42eebebbb45dc32533056c20f49fc040d483
ninaalli <ninaalli@users.noreply.github.com>
2016-04-30 17:35:14 -0400

    Update Week2Day1.py

--- Week2Day1.py
+++ Week2Day1.py
@@ -1,2 +1,5 @@
 Python with CompSci
 
+A Linked List stores data in nodes
+Each node will contain two things. A value that node is storing, and a pointer that will point to the next node in the list
+The first node in a linked list is known as the head.
