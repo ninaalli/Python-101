52a63c60cf1e52b782c04df0b8090f426396f060
ninaalli <ninaalli@users.noreply.github.com>
2016-05-10 12:47:39 -0400

    Create SentStructure.py

---
+++ SentStructure.py
@@ -0,0 +1,62 @@
+Ninas-Air:title_case_2 ninaalli$ Python3 Case2.py
+Optimus Prime And The Autobots Assembled To Fight The Decepticons
+Ninas-Air:title_case_2 ninaalli$ Python3 Case2.py
+optimus prime and the autobots assembled to fight the decepticons
+Ninas-Air:title_case_2 ninaalli$ Python3 Case2.py
+optimusprimeandtheautobotsassembledtofightthedecepticons
+Ninas-Air:title_case_2 ninaalli$ Python3 Case2.py
+ optimus prime and the autobots assembled to fight the decepticons
+Ninas-Air:title_case_2 ninaalli$ Python3 Case2.py
+optimus prime and the autobots assembled to fight the decepticons
+Ninas-Air:title_case_2 ninaalli$ Python3 Case2.py
+Optimus Prime and the Autobots Assembled to Fight the decepticons
+Ninas-Air:title_case_2 ninaalli$ git merge
+fatal: No remote for the current branch.
+Ninas-Air:title_case_2 ninaalli$ git add Desktop/MonsterAcademy/week1-day1/exercises/title_case_2/Case.py
+fatal: pathspec 'Desktop/MonsterAcademy/week1-day1/exercises/title_case_2/Case.py' did not match any files
+Ninas-Air:title_case_2 ninaalli$ git status
+On branch ninaalli
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+	modified:   ../iteration with_strings/README.md
+	modified:   ../iteration_with_lists/iteration.py
+	modified:   ../title_case_1/README.md
+	modified:   README.md
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+	Case2.py
+
+no changes added to commit (use "git add" and/or "git commit -a")
+Ninas-Air:title_case_2 ninaalli$ git add .
+Ninas-Air:title_case_2 ninaalli$ git status
+On branch ninaalli
+Changes to be committed:
+  (use "git reset HEAD <file>..." to unstage)
+
+	new file:   Case2.py
+	modified:   README.md
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+	modified:   ../iteration with_strings/README.md
+	modified:   ../iteration_with_lists/iteration.py
+	modified:   ../title_case_1/README.md
+
+Ninas-Air:title_case_2 ninaalli$ git commit -m "finally did it dk"
+[ninaalli 10bd46c] finally did it dk
+ 2 files changed, 31 insertions(+), 1 deletion(-)
+ create mode 100644 exercises/title_case_2/Case2.py
+Ninas-Air:title_case_2 ninaalli$ git push origin ninaalli
+Counting objects: 6, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (6/6), done.
+Writing objects: 100% (6/6), 816 bytes | 0 bytes/s, done.
+Total 6 (delta 3), reused 0 (delta 0)
+To https://github.com/gila-monsters-2016/week1-day1.git
+   d41a95b..10bd46c  ninaalli -> ninaalli
