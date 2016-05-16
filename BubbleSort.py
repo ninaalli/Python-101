1e52ec57c5d95f737657e177474ed1f735fe7e0c
ninaalli <ninaalli@users.noreply.github.com>
2016-05-16 13:58:08 -0400

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,11 @@
+def bubble_sort(data):
+  idx = 0
+  end_idx = len(data)-1
+  while end_idx>0:
+    idx=0
+  while idx<end_idx:
+    if val > data[idx+1]:
+    data[idx],data[idx+1] = data[idx+1], data [idx]
+    idx + = 1
+    end_idx = 1
+  print bubble_sort
