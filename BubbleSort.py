a5e69fe4954e24715fdc7b84f02c0ef5271f4b88
ninaalli <ninaalli@users.noreply.github.com>
2016-05-16 14:14:38 -0400

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -9,3 +9,22 @@ def bubble_sort(data):
     idx + = 1
     end_idx = 1
   print bubble_sort
+
+
+use the name space
+
+
+Bubble sort is defined within global namespace
+
+
+
+def bubble_sort(data):
+    end_idx = len(data)-1
+    while end_idx > 0:
+        curr_idx = 0
+        while curr_idx < end_idx:
+            if data[curr_idx] > data[curr_idx + 1]:
+                data[curr_idx], data[curr_idx + 1] = data[curr_idx + 1], data[curr_idx]
+            curr_idx += 1
+        end_idx -= 1
+    return data
