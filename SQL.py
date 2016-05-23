8775e64c8ea64e7ea00ba38baac37fdaad3b533c
ninaalli <ninaalli@users.noreply.github.com>
2016-05-23 11:50:23 -0400

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,4 +1,33 @@
-=	Equal
+
+Format of CREATE TABLE if you were to use optional constraints:
+
+create table "tablename"
+("column1" "data type" 
+         [constraint],
+ "column2" "data type" 
+         [constraint],
+ "column3" "data type" 
+        [constraint]);
+ [ ] = optional
+ 
+ Here are the most common Data types:
+
+char(size)	Fixed-length character string. Size is specified in parenthesis. Max 255 bytes.
+varchar(size)	Variable-length character string. Max size is specified in parenthesis.
+number(size)	Number value with a max number of column digits specified in parenthesis.
+date	Date value
+number(size,d)	Number value with a maximum number of digits of "size" total, with a maximum number of "d" digits to the right of the decimal.
+  Example:
+    create table employee
+(first varchar(15),
+ last varchar(20),
+ age number(3),
+ address varchar(30),
+ city varchar(20),
+ state varchar(20));
+ 
+ 
+ =	Equal
 >	Greater than
 <	Less than
 >=	Greater than or equal
@@ -6,7 +35,7 @@
 <>	Not equal to
 LIKE	*See note below
 
-
+HOW TO SEARCH:
 Display everyone's first name and their age for everyone that's in table.
 select first, 
        age 
@@ -36,19 +65,3 @@ Display all columns for everyone whose first name contains "Mary".
 select * from empinfo
        where first LIKE '%Mary%';
        
-       
-Here are the most common Data types:
-
-char(size)	Fixed-length character string. Size is specified in parenthesis. Max 255 bytes.
-varchar(size)	Variable-length character string. Max size is specified in parenthesis.
-number(size)	Number value with a max number of column digits specified in parenthesis.
-date	Date value
-number(size,d)	Number value with a maximum number of digits of "size" total, with a maximum number of "d" digits to the right of the decimal.
-  Example:
-    create table employee
-(first varchar(15),
- last varchar(20),
- age number(3),
- address varchar(30),
- city varchar(20),
- state varchar(20));
