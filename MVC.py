ada79f94d18fb2aaae04e11db07aab21ef3216b4
ninaalli <ninaalli@users.noreply.github.com>
2016-05-23 14:37:31 -0400

    Update MVC.py

--- MVC.py
+++ MVC.py
@@ -1,3 +1,43 @@
 Model - has all of the classes
 View - what the user sees
 Controller - tells the application when to execute, controller connects these things. collects things from views and sends to the model
+
+
+The Controller is the API/Interface. It gets imported into the Views to get things moving
+have to have the __init__ to initiate the call
+
+self.view is an instance of the view class, it can get any 
+
+controller interacting with the View - call it clever, display logic
+on the controller, from model import (name of the classes)
+
+whos turn it is should be and the score in the model.
+
+anything that is building strings goes in the view
+  input statements wait for the user to press return. 
+  
+model is where all the business logic, state of the application.
+player, 
+word, 
+letters guessed, correct, incorrect
+body of hangman, 
+
+doc string - lets you print it out the exact way you typed it
+print ("""
+  |---------------|
+                  |
+                  |
+                  |
+                  |
+                  |
+                -----
+""")
+
+ o
+-|-
+ |
+ /\
+
+
+
+
