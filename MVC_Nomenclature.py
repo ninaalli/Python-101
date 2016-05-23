0239d1ae20520ffb2b79a1fafc69fb84a61a361c
ninaalli <ninaalli@users.noreply.github.com>
2016-05-23 13:47:51 -0400

    Rename MVC.py to MVC_Nomenclature.py

---
+++ MVC_Nomenclature.py
@@ -0,0 +1,3 @@
+Model - has all of the classes
+View - what the user sees
+Controller - tells the application when to execute, controller connects these things. collects things from views and sends to the model
